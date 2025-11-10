a506a42c9c4e225a7dde930145ac5249ec3d49db
Aaron Wittchen <ahorn103@gmx.de>
2025-11-10 16:52:50 +0100

    ignore files

--- sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Use quotes around the path because of spaces
-rsync -avz --exclude '.git' --exclude 'logs' \
-  "/mnt/c/Users/theon/Desktop/Automated Storage Provisioning Tool/" \
-  rocky-vm@192.168.68.105:/home/rocky-vm/storage-provisioning/
